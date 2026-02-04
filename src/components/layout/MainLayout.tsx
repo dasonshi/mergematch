7e7a55ca0d174fd2886332f35bad55063b71fd9a
David Sonshine <sonshine.david@gmail.com>
2026-02-04 16:32:46 -0500

    Revert "Fix iframe cutoff: use fixed positioning for viewport-pinned layout"
    This reverts commit f44e976c1ade231f678b1ecc627d32fcdab5d25b.

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -6,7 +6,7 @@ interface MainLayoutProps {
 
 export function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="fixed inset-0 bg-background flex flex-col overflow-hidden">
+    <div className="h-[90vh] w-full bg-background flex flex-col overflow-hidden">
       <TopNav />
       <main className="flex-1 min-h-0 w-full p-4 md:p-6 lg:p-8 pb-32 overflow-y-auto">
         <div className="mx-auto max-w-screen-2xl">
